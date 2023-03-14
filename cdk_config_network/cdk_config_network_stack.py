06c9b1ec79b028df5038dc28973350c2d58e0a44
herbert <herbert.pieris@japfa.com>
2023-03-14 10:58:43 +0700

    init

--- cdk_config_network/cdk_config_network_stack.py
+++ cdk_config_network/cdk_config_network_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     # Duration,
+    aws_ec2 as _ec2,
     Stack,
     # aws_sqs as sqs,
 )
@@ -11,9 +12,17 @@ class CdkConfigNetworkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        vpcName = "japfa-poc-vpc"
+        vpcCidr = "192.168.0.0/20"
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkConfigNetworkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        #create VPC
+        vpc = _ec2.Vpc(self,
+                        vpcName,
+                        ip_addresses=_ec2.IpAddresses.cidr(vpcCidr),
+                        default_instance_tenancy=_ec2.DefaultInstanceTenancy.DEFAULT,
+                        enable_dns_hostnames=True,
+                        enable_dns_support=True,
+                        # flow_logs=
+                        vpc_name=vpcName,
+                        max_azs=2                        
+        )

0018a7dafd2a16256d184c179a260e36bb33a297
herbert <herbert.pieris@japfa.com>
2023-03-24 19:11:47 +0700

    try true false for gwlb subnet

--- cdk_config_network/cdk_config_network_stack.py
+++ cdk_config_network/cdk_config_network_stack.py
@@ -36,7 +36,7 @@ class CdkConfigNetworkStack(Stack):
         subnetPrivateGWLB1bCidr = "192.168.7.0/24"                                
         subnetAZ1a = "us-east-1a"
         subnetAZ1b = "us-east-1b"
-        OnGWLBeSubnet = True # True or Flase
+        OnGWLBeSubnet = False # True or Flase
 
         #create VPC
         vpc = _ec2.CfnVPC(
@@ -212,46 +212,47 @@ class CdkConfigNetworkStack(Stack):
         subnetPrivate1b04RouteTableAssociation.apply_removal_policy(_removalpolicy.DESTROY)
 
         ## private gwlb
-        subnetPrivateGWLB1a = _ec2.CfnSubnet(
-            self,
-            subnetPrivateGWLB1aName,
-            vpc_id = vpc.attr_vpc_id,
-            availability_zone=subnetAZ1a,
-            cidr_block=subnetPrivateGWLB1aCidr,
+        if OnGWLBeSubnet:
+            subnetPrivateGWLB1a = _ec2.CfnSubnet(
+                self,
+                subnetPrivateGWLB1aName,
+                vpc_id = vpc.attr_vpc_id,
+                availability_zone=subnetAZ1a,
+                cidr_block=subnetPrivateGWLB1aCidr,
 
-            tags=[_CfnTag(
-                key="Name",
-                value=vpcName+"-"+"subnetPrivateGWLB1a"
-            )]            
-        )
-        subnetPrivateGWLB1a.apply_removal_policy(_removalpolicy.DESTROY)
+                tags=[_CfnTag(
+                    key="Name",
+                    value=vpcName+"-"+"subnetPrivateGWLB1a"
+                )]            
+            )
+            subnetPrivateGWLB1a.apply_removal_policy(_removalpolicy.DESTROY)
 
-        subnetPrivateGWLB1aRouteTableAssociation = _ec2.CfnSubnetRouteTableAssociation(
-            self,
-            "subnetPrivateGWLB1aRouteTableAssociation",
-            route_table_id=route_tablePrivate.ref,
-            subnet_id=subnetPrivateGWLB1a.attr_subnet_id
-        )
-        subnetPrivateGWLB1aRouteTableAssociation.apply_removal_policy(_removalpolicy.DESTROY)
+            subnetPrivateGWLB1aRouteTableAssociation = _ec2.CfnSubnetRouteTableAssociation(
+                self,
+                "subnetPrivateGWLB1aRouteTableAssociation",
+                route_table_id=route_tablePrivate.ref,
+                subnet_id=subnetPrivateGWLB1a.attr_subnet_id
+            )
+            subnetPrivateGWLB1aRouteTableAssociation.apply_removal_policy(_removalpolicy.DESTROY)
 
-        subnetPrivateGWLB1b = _ec2.CfnSubnet(
-            self,
-            subnetPrivateGWLB1bName,
-            vpc_id = vpc.attr_vpc_id,
-            availability_zone=subnetAZ1b,
-            cidr_block=subnetPrivateGWLB1bCidr,
+            subnetPrivateGWLB1b = _ec2.CfnSubnet(
+                self,
+                subnetPrivateGWLB1bName,
+                vpc_id = vpc.attr_vpc_id,
+                availability_zone=subnetAZ1b,
+                cidr_block=subnetPrivateGWLB1bCidr,
 
-            tags=[_CfnTag(
-                key="Name",
-                value=vpcName+"-"+"subnetPrivateGWLB1b"
-            )]            
-        )
-        subnetPrivateGWLB1b.apply_removal_policy(_removalpolicy.DESTROY)
+                tags=[_CfnTag(
+                    key="Name",
+                    value=vpcName+"-"+"subnetPrivateGWLB1b"
+                )]            
+            )
+            subnetPrivateGWLB1b.apply_removal_policy(_removalpolicy.DESTROY)
 
-        subnetPrivateGWLB1bRouteTableAssociation = _ec2.CfnSubnetRouteTableAssociation(
-            self,
-            "subnetPrivateGWLB1bRouteTableAssociation",
-            route_table_id=route_tablePrivate.ref,
-            subnet_id=subnetPrivateGWLB1b.attr_subnet_id
-        )
-        subnetPrivateGWLB1bRouteTableAssociation.apply_removal_policy(_removalpolicy.DESTROY)                        
+            subnetPrivateGWLB1bRouteTableAssociation = _ec2.CfnSubnetRouteTableAssociation(
+                self,
+                "subnetPrivateGWLB1bRouteTableAssociation",
+                route_table_id=route_tablePrivate.ref,
+                subnet_id=subnetPrivateGWLB1b.attr_subnet_id
+            )
+            subnetPrivateGWLB1bRouteTableAssociation.apply_removal_policy(_removalpolicy.DESTROY)                        
